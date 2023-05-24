dd32b15ff5864b053221f14887098954c1e2a5a9
amitaka123 <amitkumaranandaka@gmail.com>
2023-05-24 15:26:21 +0530

    files update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const userRouter = require('./routes/user-router')
 main().catch(err => console.log(err))
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
+  await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce2');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
   console.log('database connected');
